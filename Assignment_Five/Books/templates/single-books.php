f95c59754f830443d532091a1853b279e982e081
Devshakhawat <shakhawat579@gmail.com>
2021-03-14 00:00:59 +0600

    modifed assignment five

--- Assignment_Five/Books/templates/single-books.php
+++ Assignment_Five/Books/templates/single-books.php
@@ -2,21 +2,21 @@
 
 get_header();
 
- while ( have_posts() ) {
-	the_post();
-	$postID = get_the_ID();
+while ( have_posts() ) {
+    the_post();
+    $postID = esc_html( get_the_ID() );
 
-	$writer_name  = get_post_meta( $postID, 'writer_name', true );
-	$wdp_pub_name = get_post_meta( $postID, 'wdp_pub_name', true );
-	$wdp_isbn_no  = get_post_meta( $postID, 'wdp_isbn_no', true );
-	$wdp_pdate    = get_post_meta( $postID, 'wdp_pdate', true );
-	$wdp_email    = get_post_meta( $postID, 'wdp_email', true );
+    $writer_name  = esc_html( get_post_meta( $postID, 'writer_name', true ) );
+    $wdp_pub_name = esc_html( get_post_meta( $postID, 'wdp_pub_name', true ) );
+    $wdp_isbn_no  = esc_html( get_post_meta( $postID, 'wdp_isbn_no', true ) );
+    $wdp_pdate    = esc_html( get_post_meta( $postID, 'wdp_pdate', true ) );
+    $wdp_email    = esc_html( get_post_meta( $postID, 'wdp_email', true ) );
 
-	echo $writer_name .'<br>';
-	echo $wdp_pub_name .'<br>';
-	echo $wdp_isbn_no .'<br>';
-	echo $wdp_pdate . '<br>';
-	echo $wdp_email;
+    echo $writer_name . '<br>';
+    echo $wdp_pub_name . '<br>';
+    echo $wdp_isbn_no . '<br>';
+    echo $wdp_pdate . '<br>';
+    echo $wdp_email;
 }
 
 get_footer();
