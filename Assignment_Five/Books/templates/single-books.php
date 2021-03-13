270b254582055f99179597b49f1baec975fa6055
Shakhawat Hossain <shakhawat579@gmail.com>
2021-03-13 23:47:38 +0600

    Update single-books.php

--- Assignment_Five/Books/templates/single-books.php
+++ Assignment_Five/Books/templates/single-books.php
@@ -6,11 +6,11 @@ get_header();
 	the_post();
 	$postID = get_the_ID();
 
-	$writer_name = get_post_meta( $postID, 'writer_name', true );
+	$writer_name  = get_post_meta( $postID, 'writer_name', true );
 	$wdp_pub_name = get_post_meta( $postID, 'wdp_pub_name', true );
-	$wdp_isbn_no = get_post_meta( $postID, 'wdp_isbn_no', true );
-	$wdp_pdate = get_post_meta( $postID, 'wdp_pdate', true );
-	$wdp_email = get_post_meta( $postID, 'wdp_email', true );
+	$wdp_isbn_no  = get_post_meta( $postID, 'wdp_isbn_no', true );
+	$wdp_pdate    = get_post_meta( $postID, 'wdp_pdate', true );
+	$wdp_email    = get_post_meta( $postID, 'wdp_email', true );
 
 	echo $writer_name .'<br>';
 	echo $wdp_pub_name .'<br>';
